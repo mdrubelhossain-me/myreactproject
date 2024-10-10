a5eb6823b9fe1391ffb42051548a5f0f2c1d95d7
mdrubelhossain-me <mdrubelhossain.cse@gmail.com>
2024-10-11 00:14:11 +0600

    Product add Dynamicallyy

--- src/Components/FlashSale/FlashSale.jsx
+++ src/Components/FlashSale/FlashSale.jsx
@@ -1,15 +1,7 @@
 import React from "react";
 import "./FlashSale.css";
 import "./FlashSaleResponsive.css";
-import p1 from "../../assets/images/Product/apple.png";
-import p2 from "../../assets/images/Product/banana.png";
-import p3 from "../../assets/images/Product/carrots.png";
-import p4 from "../../assets/images/Product/cauliflower.png";
-import p5 from "../../assets/images/Product/cumcumber.png";
-import p6 from "../../assets/images/Product/egg.png";
-import p7 from "../../assets/images/Product/grapes.png";
-import p8 from "../../assets/images/Product/potato.png";
-import p9 from "../../assets/images/Product/tomato.png";
+import products from "../../data/ProductData";
 
 const FlashSale = () => {
   return (
@@ -22,306 +14,38 @@ const FlashSale = () => {
             ducimus quis quaerat necessitatibus commodi.
           </p>
           <div className="row g-3 mt-3">
-            {/* Single */}
-            <div className="col-lg-2 col-md-4 col-sm-6 col-6">
-              <div className="card">
-                <div className="product-photo">
-                  <img src={p1} alt="" />
-                </div>
-                <div className="card-body">
-                  <a href="" className="product-title d-block m-0 p-0">
-                    Fresh Apple
-                  </a>
-                  <div className="ratting m-0 p-0">
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-half"></i>
+            {products.map((product) => (
+              <div
+                className="col-lg-2 col-md-4 col-sm-6 col-6"
+                key={product.id}
+              >
+                <div className="card">
+                  <div className="product-photo">
+                    <img src={product.imgSrc} alt={product.title} />
                   </div>
-                  <div className="price">
-                    <h6>৳200 kg</h6>
-                    <span>৳250</span>
-                    <del className="mx-2">30%</del>
-                  </div>
-                </div>
-              </div>
-            </div>
-            {/* Single */}
-            <div className="col-lg-2 col-md-4 col-sm-6 col-6">
-              <div className="card">
-                <div className="product-photo">
-                  <img src={p2} alt="" />
-                </div>
-                <div className="card-body">
-                  <a href="" className="product-title d-block m-0 p-0">
-                    Fresh Banana
-                  </a>
-                  <div className="ratting m-0 p-0">
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-half"></i>
-                  </div>
-                  <div className="price">
-                    <h6>৳40 </h6>
-                    <span>৳50</span>
-                    <del className="mx-2">10%</del>
+                  <div className="card-body">
+                    <a href="" className="product-title d-block m-0 p-0">
+                      {product.title}
+                    </a>
+                    <div className="rating m-0 p-0">
+                      {[...Array(Math.floor(product.rating))].map(
+                        (_, index) => (
+                          <i className="bi bi-star-fill" key={index} style={{ color: 'orange', fontSize: '12px' }}></i>
+                        )
+                      )}
+                      {product.rating % 1 !== 0 && (
+                        <i className="bi bi-star-half" style={{ color: 'orange', fontSize: '12px' }}></i>
+                      )}
+                    </div>
+                    <div className="price">
+                      <h6>৳{product.price} kg</h6>
+                      <span>৳{product.originalPrice}</span>
+                      <del className="mx-2">{product.discount}%</del>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-            {/* Single */}
-            <div className="col-lg-2 col-md-4 col-sm-6 col-6">
-              <div className="card">
-                <div className="product-photo">
-                  <img src={p3} alt="" />
-                </div>
-                <div className="card-body">
-                  <a href="" className="product-title d-block m-0 p-0">
-                    Fresh Carrots
-                  </a>
-                  <div className="ratting m-0 p-0">
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-half"></i>
-                  </div>
-                  <div className="price">
-                    <h6>৳200 kg</h6>
-                    <span>৳250</span>
-                    <del className="mx-2">30%</del>
-                  </div>
-                </div>
-              </div>
-            </div>
-            {/* Single */}
-            <div className="col-lg-2 col-md-4 col-sm-6 col-6">
-              <div className="card">
-                <div className="product-photo">
-                  <img src={p4} alt="" />
-                </div>
-                <div className="card-body">
-                  <a href="" className="product-title d-block m-0 p-0">
-                    Fresh Cauliflower
-                  </a>
-                  <div className="ratting m-0 p-0">
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-half"></i>
-                  </div>
-                  <div className="price">
-                    <h6>৳100 kg</h6>
-                    <span>৳150</span>
-                    <del className="mx-2">30%</del>
-                  </div>
-                </div>
-              </div>
-            </div>
-            {/* Single */}
-            <div className="col-lg-2 col-md-4 col-sm-6 col-6">
-              <div className="card">
-                <div className="product-photo">
-                  <img src={p5} alt="" />
-                </div>
-                <div className="card-body">
-                  <a href="" className="product-title d-block m-0 p-0">
-                    Fresh Cucumber
-                  </a>
-                  <div className="ratting m-0 p-0">
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-half"></i>
-                  </div>
-                  <div className="price">
-                    <h6>৳70 kg</h6>
-                    <span>৳150</span>
-                    <del className="mx-2">40%</del>
-                  </div>
-                </div>
-              </div>
-            </div>
-            {/* Single */}
-            <div className="col-lg-2 col-md-4 col-sm-6 col-6">
-              <div className="card">
-                <div className="product-photo">
-                  <img src={p6} alt="" />
-                </div>
-                <div className="card-body">
-                  <a href="" className="product-title d-block m-0 p-0">
-                    Egg
-                  </a>
-                  <div className="ratting m-0 p-0">
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-half"></i>
-                  </div>
-                  <div className="price">
-                    <h6>৳200</h6>
-                    <span>৳250</span>
-                    <del className="mx-2">30%</del>
-                  </div>
-                </div>
-              </div>
-            </div>
-            {/* Single */}
-            <div className="col-lg-2 col-md-4 col-sm-6 col-6">
-              <div className="card">
-                <div className="product-photo">
-                  <img src={p7} alt="" />
-                </div>
-                <div className="card-body">
-                  <a href="" className="product-title d-block m-0 p-0">
-                    Fresh Grapes
-                  </a>
-                  <div className="ratting m-0 p-0">
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-half"></i>
-                  </div>
-                  <div className="price">
-                    <h6>৳200 kg</h6>
-                    <span>৳250</span>
-                    <del className="mx-2">30%</del>
-                  </div>
-                </div>
-              </div>
-            </div>
-            {/* Single */}
-            <div className="col-lg-2 col-md-4 col-sm-6 col-6">
-              <div className="card">
-                <div className="product-photo">
-                  <img src={p8} alt="" />
-                </div>
-                <div className="card-body">
-                  <a href="" className="product-title d-block m-0 p-0">
-                    Fresh Potato
-                  </a>
-                  <div className="ratting m-0 p-0">
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-half"></i>
-                  </div>
-                  <div className="price">
-                    <h6>৳100 kg</h6>
-                    <span>৳200</span>
-                    <del className="mx-2">50%</del>
-                  </div>
-                </div>
-              </div>
-            </div>
-            {/* Single */}
-            <div className="col-lg-2 col-md-4 col-sm-6 col-6">
-              <div className="card">
-                <div className="product-photo">
-                  <img src={p9} alt="" />
-                </div>
-                <div className="card-body">
-                  <a href="" className="product-title d-block m-0 p-0">
-                    Fresh Tomato
-                  </a>
-                  <div className="ratting m-0 p-0">
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-half"></i>
-                  </div>
-                  <div className="price">
-                    <h6>৳100 kg</h6>
-                    <span>৳200</span>
-                    <del className="mx-2">50%</del>
-                  </div>
-                </div>
-              </div>
-            </div>
-            {/* Single */}
-            <div className="col-lg-2 col-md-4 col-sm-6 col-6">
-              <div className="card">
-                <div className="product-photo">
-                  <img src={p1} alt="" />
-                </div>
-                <div className="card-body">
-                  <a href="" className="product-title d-block m-0 p-0">
-                    Fresh Apple
-                  </a>
-                  <div className="ratting m-0 p-0">
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-half"></i>
-                  </div>
-                  <div className="price">
-                    <h6>৳200 kg</h6>
-                    <span>৳250</span>
-                    <del className="mx-2">30%</del>
-                  </div>
-                </div>
-              </div>
-            </div>
-            {/* Single */}
-            <div className="col-lg-2 col-md-4 col-sm-6 col-6">
-              <div className="card">
-                <div className="product-photo">
-                  <img src={p2} alt="" />
-                </div>
-                <div className="card-body">
-                  <a href="" className="product-title d-block m-0 p-0">
-                    Fresh Banana
-                  </a>
-                  <div className="ratting m-0 p-0">
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-half"></i>
-                  </div>
-                  <div className="price">
-                    <h6>৳40 </h6>
-                    <span>৳50</span>
-                    <del className="mx-2">10%</del>
-                  </div>
-                </div>
-              </div>
-            </div>
-            {/* Single */}
-            <div className="col-lg-2 col-md-4 col-sm-6 col-6">
-              <div className="card">
-                <div className="product-photo">
-                  <img src={p3} alt="" />
-                </div>
-                <div className="card-body">
-                  <a href="" className="product-title d-block m-0 p-0">
-                    Fresh Carrots
-                  </a>
-                  <div className="ratting m-0 p-0">
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-half"></i>
-                  </div>
-                  <div className="price">
-                    <h6>৳200 kg</h6>
-                    <span>৳250</span>
-                    <del className="mx-2">30%</del>
-                  </div>
-                </div>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
---
+++ src/data/ProductData.jsx
@@ -0,0 +1,96 @@
+import p1 from  '../assets/images/Product/carrots.png'
+import p2 from  '../assets/images/Product/apple.png'
+import p3 from  '../assets/images/Product/banana.png'
+import p4 from  '../assets/images/Product/cauliflower.png'
+import p5 from  '../assets/images/Product/cumcumber.png'
+import p6 from  '../assets/images/Product/egg.png'
+import p7 from  '../assets/images/Product/grapes.png'
+import p8 from  '../assets/images/Product/potato.png'
+import p9 from  '../assets/images/Product/tomato.png'
+
+
+const products = [
+    {
+        id: 1,
+        imgSrc: p1,
+        title: "Fresh Carrots",
+        rating: 4.5, // For example, 4 full stars and a half star
+        price: 200,
+        originalPrice: 250,
+        discount: 30
+    },
+    {
+        id: 2,
+        imgSrc: p2,
+        title: "Fresh Apple",
+        rating: 4, // For example, 4 full stars and a half star
+        price: 200,
+        originalPrice: 260,
+        discount: 20
+    },
+    {
+        id: 3,
+        imgSrc: p3,
+        title: "Fresh Banana",
+        rating: 4.0, // For example, 4 full stars and a half star
+        price: 100,
+        originalPrice: 250,
+        discount: 30
+    },
+    {
+        id: 4,
+        imgSrc: p4,
+        title: "Fresh Cauliflower",
+        rating: 5, // For example, 4 full stars and a half star
+        price: 180,
+        originalPrice: 240,
+        discount: 25
+    },
+    {
+        id: 5,
+        imgSrc: p5,
+        title: "Fresh Cucumber",
+        rating: 4.5, // For example, 4 full stars and a half star
+        price: 200,
+        originalPrice: 250,
+        discount: 30
+    },
+    {
+        id: 6,
+        imgSrc: p6,
+        title: "Fresh Egg",
+        rating: 3.5, // For example, 4 full stars and a half star
+        price: 180,
+        originalPrice: 200,
+        discount: 10
+    },
+    {
+        id: 7,
+        imgSrc: p7,
+        title: "Fresh Grapes",
+        rating: 4.5, // For example, 4 full stars and a half star
+        price: 200,
+        originalPrice: 250,
+        discount: 30
+    },
+    {
+        id: 8,
+        imgSrc: p8,
+        title: "Fresh Potato",
+        rating: 4.5, // For example, 4 full stars and a half star
+        price: 100,
+        originalPrice: 120,
+        discount: 15
+    },
+    {
+        id: 9,
+        imgSrc: p9,
+        title: "Fresh Tomato",
+        rating: 4.5, // For example, 4 full stars and a half star
+        price: 120,
+        originalPrice: 150,
+        discount: 15
+    }
+];
+
+export default products;
